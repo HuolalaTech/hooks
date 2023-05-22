b3b4b92ed468cb5610f55cd910ea2bf6dcacb176
YanagiEiichi <576398868@qq.com>
2023-05-22 14:07:51 +0800

    Update comments

--- src/use-event-listener/index.ts
+++ src/use-event-listener/index.ts
@@ -18,18 +18,44 @@ export function useEventListener<E extends Event = Event>(
   options: Options = {},
 ) {
   const { target = window, passive, capture } = options;
+
+  // In fact, some developer usually wite code like this
+  //
+  // ```js
+  // useEventListener('mousemove', (event) => {
+  //   // TODO
+  // });
+  // ```
+  //
+  // Note that `listener` is a literal function, which means that it always be different function object reference.
+  // As a dependency of useMemo/useEffect, passing in a different reference will be considered as an update.
+  // Therefore, using the `listener` directly as dependency may result in a high frequency of compnent updates.
+  //
+  // This `handler` is the same reference in the same component instance.
+  // Even if the passed `listener` is changed at a high frequency, the `handler` still not be changed.
   const ref = useRef<Handler<E>>();
   ref.current = listener;
   const handler = useCallback((e: Event) => {
-    if (!ref.current) return;
+    if (typeof ref.current !== 'function') return;
     ref.current(e as E);
   }, []);
+
+  // Why put the addEventListener in useMemo rather than useEffect? Because the useEffect is executed too late.
+  // If a listener is added after useEffect, some events may be missed, for example
+  //
+  // ```js
+  // const obj = useMemo(() => new MyClass());
+  // useEventListener('myevent', () => 'TODO', { target: obj });
+  // ```
+  //
+  // MyClass may dispatch an initial event in the next tick, which is triggered before useEffect.
   useMemo(() => {
-    target?.addEventListener(eventName, handler, { passive, capture });
+    target.addEventListener(eventName, handler, { passive, capture });
   }, [target, eventName, passive, capture]);
+
   useEffect(() => {
     return () => {
-      target?.removeEventListener(eventName, handler, { capture });
+      target.removeEventListener(eventName, handler, { capture });
     };
   }, [target, eventName, passive, capture]);
 }
